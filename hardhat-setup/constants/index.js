85c544be1b45eff2b0cbfea6ba244a2c0fc93193
aswinraj <aswinrajkailath@gmail.com>
2022-07-31 16:37:42 +0530

    Added API for opensea

--- hardhat-setup/constants/index.js
+++ hardhat-setup/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "address-of-the-whitelist-contract";
+const WHITELIST_CONTRACT_ADDRESS = "0x65B969aA4A4a8B8aaA14eCc9Eab13335907cA25b";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "";
+const METADATA_URL = "https://nft-collection-nine-beta.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
